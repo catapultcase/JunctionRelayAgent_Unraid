1014567765c8fc30c0d325d8b5787a4d349cf956
Rusty6285 <rustyone@gmail.com>
2024-06-13 10:56:06 -0500

    real sensors

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import json
-import random
+import psutil
 from flask import Flask, request
 
 app = Flask(__name__)
 
-def get_fake_system_info():
-    # Generate fake CPU load value (between 0 and 100)
-    cpu_load = str(random.randint(0, 100))
+def get_system_info():
+    # Get CPU load
+    cpu_load = psutil.cpu_percent(interval=1)
 
-    # Generate fake memory usage value (between 0 and 100)
-    memory_usage = str(random.randint(0, 100))
+    # Get memory usage
+    memory = psutil.virtual_memory()
+    memory_usage = memory.percent
+
+    # Get disk usage
+    disk = psutil.disk_usage('/')
+    disk_usage = disk.percent
+
+    # Get CPU temperature
+    temperatures = psutil.sensors_temperatures()
+    cpu_temp = None
+    if 'coretemp' in temperatures:
+        cpu_temp = temperatures['coretemp'][0].current
 
     # Construct the system information JSON
     info = {
@@ -28,8 +39,19 @@ def get_fake_system_info():
                                     {
                                         "Text": "CPU Load",
                                         "Type": "Load",
-                                        "Value": cpu_load,
+                                        "Value": str(cpu_load),
                                         "SensorId": "cpu_load"
+                                    }
+                                ]
+                            },
+                            {
+                                "Text": "Temperature",
+                                "Children": [
+                                    {
+                                        "Text": "CPU Temperature",
+                                        "Type": "Temperature",
+                                        "Value": str(cpu_temp) if cpu_temp is not None else "N/A",
+                                        "SensorId": "cpu_temperature"
                                     }
                                 ]
                             }
@@ -44,8 +66,24 @@ def get_fake_system_info():
                                     {
                                         "Text": "Memory Usage",
                                         "Type": "Memory",
-                                        "Value": memory_usage,
+                                        "Value": str(memory_usage),
                                         "SensorId": "memory_usage"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "Text": "Disk",
+                        "Children": [
+                            {
+                                "Text": "Usage",
+                                "Children": [
+                                    {
+                                        "Text": "Disk Usage",
+                                        "Type": "Disk",
+                                        "Value": str(disk_usage),
+                                        "SensorId": "disk_usage"
                                     }
                                 ]
                             }
@@ -59,12 +97,12 @@ def get_fake_system_info():
 
 @app.route('/system-info', methods=['GET'])
 def system_info():
-    info = get_fake_system_info()
+    info = get_system_info()
     return json.dumps(info)
 
 @app.route('/data.json', methods=['GET'])
 def data():
-    info = get_fake_system_info()
+    info = get_system_info()
     return json.dumps(info)
 
 @app.route('/receive-data', methods=['POST'])
